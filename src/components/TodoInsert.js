afaf5d1c2481483cdff300574462ab1e67031756
DoyeonKim <doyeon.kim0208@gmail.com>
2020-01-31 22:29:36 +0900

    200131  - 리액트 10장 일정 관리 웹 애플리케이션 만들기  - 단순 실습

---
+++ src/components/TodoInsert.js
@@ -0,0 +1,33 @@
+import React, { useState, useCallback } from 'react';
+import { MdAddBox } from 'react-icons/md';
+import './TodoInsert.scss';
+
+const TodoInsert = ({ onInsert }) => {
+  const [value, setValue] = useState('');
+
+  const onChange = useCallback(e => {
+    setValue(e.target.value);
+    console.log(e.target.value);
+  }, []);
+  const onSubmit = useCallback(
+    e => {
+      onInsert(value);
+      setValue('');
+      e.preventDefault();
+    },
+    [onInsert, value],
+  );
+  return (
+    <form className="TodoInsert" onSubmit={onSubmit}>
+      <input
+        placeholder="할 일을 입력하세요"
+        value={value}
+        onChange={onChange}
+      />
+      <button type="submit">
+        <MdAddBox />
+      </button>
+    </form>
+  );
+};
+export default TodoInsert;
